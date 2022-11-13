7da5c2272585f11372c7f1b4b6f8c4f4eb184e00
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2022-11-13 11:18:50 +0530

    Structure

---
+++ app1.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const session = require('express-session');
+const passport = require('passport');
+const nodemailer = require("nodemailer");
+const findOrCreate = require('mongoose-findorcreate')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passportLocalMongoose = require('passport-local-mongoose');
+const mongoose =require('mongoose');
+const bcrypt = require("bcrypt");
+
+ const port = 3000||process.env.PORT;
+const app =express();
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"));
+app.use(express.json());
+app.set("view engine","ejs");
+app.get("/",function(req,res){
+    res.render("recaptcha")
+})
+app.post("/",function(req,res)
+{
+    const {email,password} = req.body;
+    res.redirect("/login")
+});
+app.listen(port,function(err){
+    if(err)console.log(err);
+    else console.log("Server Started");
+})
